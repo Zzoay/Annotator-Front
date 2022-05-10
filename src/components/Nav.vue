dcee60dc195a5dc6cc5b01f9f9694d5e5efae8ba
Zzoay <jianggongyao@gmail.com>
2022-05-10 13:19:35 +0800

    feat: add nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+    <a class="navbar-brand" href="#">&nbsp; 标注平台</a>
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+    </ul>
+    </nav>
+
+</template>
+
+<style>
+.navbar {
+    background-color: #fff;
+    border-bottom: 1px solid #ccc;
+    border-radius: 1px;
+}
+</style>
